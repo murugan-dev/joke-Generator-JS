32d08d3158dc762d0ffc25cdceb82f01ae1fc2aa
murugan-dev <muruganatdev@gmail.com>
2024-02-06 20:16:34 +0530

    updating relative names

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const imgVisible = document.querySelector("#imgTag")
 console.log(imgVisible)
 document.querySelector(".btn").addEventListener("click", joke);
 async function joke() {
+
     let config ={
         headers : {
             Accept : "application/json",
     },
 };
-let a =await fetch("https://icanhazdadjoke.com/", config);
-console.log(a)
-let b = await a.json();
-console.log(b)
-document.querySelector("#content").innerHTML= b.joke;
+
+let data =await fetch("https://icanhazdadjoke.com/", config);
+console.log(data)
+let jsonValue = await data.json();
+console.log(jsonValue)
+document.querySelector("#content").innerHTML= jsonValue.joke;
+
 imgVisible.classList.add("imgVisible")
 
 }
